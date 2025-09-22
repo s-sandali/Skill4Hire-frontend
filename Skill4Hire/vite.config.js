7c7d8c57d4f4c4b58b083a7b379cd1f4cbb87d22
Sasindi-B <sasindi.bandara2003@gmail.com>
2025-09-22 16:02:04 +0530

    SCRUM 101 candidate features

--- Skill4Hire/vite.config.js
+++ Skill4Hire/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5714,
+    proxy: {
+      // forward /api/* requests to backend running on 8000
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
