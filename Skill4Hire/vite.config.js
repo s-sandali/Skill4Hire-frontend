66f906973676f149e624122b1736d5b4fe1e7b9e
Sasindi-B <sasindi.bandara2003@gmail.com>
2025-09-23 00:26:23 +0530

    SCRUM-101 Status of applied jobs add

--- Skill4Hire/vite.config.js
+++ Skill4Hire/vite.config.js
@@ -5,11 +5,11 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5714,
+    port: 5175,
     proxy: {
       // forward /api/* requests to backend running on 8000
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
       },
